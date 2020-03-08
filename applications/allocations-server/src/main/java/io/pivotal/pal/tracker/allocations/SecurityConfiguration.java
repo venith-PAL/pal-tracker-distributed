fe2da2744634ab0435cb9307062828d0972c7a37
venith-PAL <venith.thayyil@cognizant.com>
2020-03-08 11:43:33 +0530

    2nd assignment

--- applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/SecurityConfiguration.java
+++ applications/allocations-server/src/main/java/io/pivotal/pal/tracker/allocations/SecurityConfiguration.java
@@ -5,8 +5,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-
-
     @EnableWebSecurity
     @Configuration
     public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
